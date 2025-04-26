57694eec596c498a6a03afdf2fbaa0b78192d9f2
Lewis <lew.brougham@gmail.com>
2025-04-26 20:59:38 +0100

    map api improve

--- js/map.js
+++ js/map.js
@@ -1,40 +1,36 @@
-// Initialize the map centered on Ormskirk
-var map = L.map('map').setView([53.5656, -2.8941], 13); // Centered on Ormskirk
 
-// Add OpenStreetMap tile layer
+var map = L.map('map').setView([53.5656, -2.8941], 13); 
+
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-// Custom Material Icon for the pins (using the restaurant icon as an example)
 var locationIcon = L.divIcon({
     className: 'material-icons',
-    iconSize: [40, 40],  // Adjust size of the icon
-    iconAnchor: [20, 40],  // Anchor the icon at the bottom
-    popupAnchor: [0, -40],  // Position the popup above the pin
-    html: '<span class="material-icons" style="font-size: 30px; color: black;">place</span>', // Material icon for location pin
+    iconSize: [40, 40],  
+    iconAnchor: [20, 40],  
+    popupAnchor: [0, -40],  
+    html: '<span class="material-icons" style="font-size: 30px; color: black;">pets</span>', 
 });
 
-// Locations array
 var locations = [
     {
         name: "Doggie Walks Ormskirk",
         address: "3-5 St Helens Road, Ormskirk, L35 4PO",
-        coords: [53.5656, -2.8941], // Ormskirk coordinates
+        coords: [57.4718, -2.9835], 
     },
     {
         name: "Doggie Walks Southport",
         address: "150 Lord Street, Southport, PR9 0NP",
-        coords: [53.6445, -3.0043], // Southport coordinates
+        coords: [53.7173, -2.9957], 
     },
     {
         name: "Doggie Walks Warrington",
         address: "47 Old Liverpool Road, Warrington, WA5 1AF",
-        coords: [53.3894, -2.5980], // Warrington coordinates
+        coords: [53.3883, -2.6093], 
     }
 ];
 
-// Add markers for each location
 locations.forEach(function(location) {
     L.marker(location.coords, { icon: locationIcon }).addTo(map)
         .bindPopup(`
